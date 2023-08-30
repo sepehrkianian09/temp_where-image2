595ba64d6eb26ce0b68319f6eb626d19f6623751
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2023-08-30 18:48:33 +0430

    feat(core): init install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+pip install markdown
+pip install protobuf==3.19.6
+pip install .\tensorflow-1.2.1-cp36-cp36m-win_amd64.whl
+pip install future
+pip install scipy
+pip install h5py
+pip install hyperopt==0.1.0
+pip install -r .\requirements_gpyopt.txt
+pip install GPyOpt==1.2.6

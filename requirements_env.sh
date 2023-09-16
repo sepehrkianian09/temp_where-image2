a7a4b616b0ef16198733fb53c6cfded83d3a391e
Sepehr Kianian <sepehrkianian09@yahoo.com>
2023-09-16 17:21:06 +0330

    fix(): fix shs

--- requirements_env.sh
+++ requirements_env.sh
@@ -1,16 +1,17 @@
 # install python 2.7
 # https://linux.how2shout.com/how-to-install-python-2-7-on-ubuntu-20-04-lts/#1_Open_a_command_terminal
-sudo apt update
-sudo apt install python2-minimal
+apt update
+
+apt install -y python2-minimal
 python2 -V
 ls /usr/bin/python*
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 2
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+update-alternatives --install /usr/bin/python python /usr/bin/python2 2
+update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 echo "python command example"
 python -V
-sudo apt install curl 
+apt install -y curl 
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-sudo python2 get-pip.py
+python2 get-pip.py
 pip2 --version
 
 # install venv
@@ -20,13 +21,14 @@ virtualenv -p $(which python) .venv
 source .venv/bin/activate
 
 # install java
-sudo apt install openjdk-8-jdk
+apt install -y openjdk-8-jdk
 java -version
 
 # install dependencies
-sudo apt-get install python-dev
-sudo apt-get install -y python-tk
+apt-get install -y python-dev
+apt-get install -y python-tk
 
 # clone first
-git clone git@github.com:tylin/coco-caption.git tools
-./tools/coco-caption-master/get_stanford_models.sh
+apt install -y git
+git clone https://github.com/tylin/coco-caption.git tools/coco-caption-master
+./tools/coco-caption/get_stanford_models.sh
